bbe25ecda2cdf4e0c244c0c79e08951e9148fd50
Сергей Борисов <openself@bk.ru>
2018-01-16 11:35:38 +0300

    Устранены замечания: 1. Дублируется ‘access_token’: TOKEN, ‘v’: VERSION 2. Дублируется https://api.vk.com/method 3. Дублируется обработка ошибок/статуса ответов api 4. is_there_any_friend содержит логику повторных запросов, хотя она является общей

--- homework/diploma/diploma_bsn.py
+++ homework/diploma/diploma_bsn.py
@@ -6,8 +6,9 @@ import time
 from sys import stdout
 import json
 
-VERSION = '5.69'
-TOKEN = '5dfd6b0dee902310df772082421968f4c06443abecbc082a8440cb18910a56daca73ac8d04b25154a1128'
+base_params = {'access_token': '5dfd6b0dee902310df772082421968f4c06443abecbc082a8440cb18910a56daca73ac8d04b25154a1128',
+               'v': '5.69'}
+base_url = 'https://api.vk.com/method/'
 
 def create_generator():
     i = 0
@@ -22,17 +23,36 @@ def show_progress(gen):
     stdout.flush()
 
 
-def get_service_response(url, params):
+def get_service_response(url, params, progress_gen):
     response_json = None
-    try:
-        response = requests.get(url, params)
-        if response.status_code == 200:
-            response_json = response.json()
-        else:
+
+    for attempt_no in range(10):
+        show_progress(progress_gen)
+
+        try:
+            response = requests.get(url, params)
+            if response.status_code == 200:
+                response_json = response.json()
+                if 'error' in response_json:
+                    if response_json['error']['error_code'] == 113:
+                        print('Ошибка: не найден пользователь ВК по введенным данным.')
+                        break
+                    elif response_json['error']['error_code'] == 6:
+                        time.sleep(0.5)
+                    else:
+                        print('Ошибка запроса к сервису ВК.')
+                        print('Системная ошибка:', response_json['error']['error_msg'])
+                else:
+                    break
+            else:
+                print('Ошибка доступа к сервису ВК. Код статуса: ' + response.status_code)
+
+        except Exception as err:
             print('Ошибка доступа к сервису ВК.')
-    except Exception as err:
-        print('Ошибка доступа к сервису ВК.')
-        print('Системная ошибка:', str(err))
+            print('Системная ошибка:', str(err))
+
+    if response_json and 'error' in response_json:
+        response_json = None
 
     return response_json
 
@@ -40,24 +60,12 @@ def get_service_response(url, params):
 def get_user_id(user_data, progress_gen):
     '''Возвращает идентификатор пользователя'''
     user_id = None
-    params = {
-        'access_token': TOKEN,
-        'v': VERSION,
-        'user_ids': user_data
-    }
+    spec_params = {'user_ids': user_data}
+    params = {**base_params, ** spec_params}
+    response_json = get_service_response(base_url + 'users.get', params, progress_gen)
 
-    show_progress(progress_gen)
-
-    response_json = get_service_response('https://api.vk.com/method/users.get', params)
     if response_json:
-        if 'error' in response_json:
-            if response_json['error']['error_code'] == 113:
-                print('Ошибка: не найден пользователь ВК по введенным данным.')
-            else:
-                print('Ошибка запроса к сервису ВК.')
-                print('Системная ошибка:', response_json['error']['error_msg'])
-        else:
-            user_id = response_json['response'][0]['id']
+        user_id = response_json['response'][0]['id']
 
     return user_id
 
@@ -65,25 +73,14 @@ def get_user_id(user_data, progress_gen):
 def get_user_groups(user_id, progress_gen):
     '''Возвращает список групп пользователя'''
     groups_list = None
-    params = {
-        'access_token': TOKEN,
-        'v': VERSION,
-        'user_id': user_id,
-        'extended': 1,
-        'fields': 'members_count'
-    }
+    spec_params = {'user_id': user_id, 'extended': 1, 'fields': 'members_count'}
+    params = {**base_params, **spec_params}
+    response_json = get_service_response(base_url + 'groups.get', params, progress_gen)
 
-    show_progress(progress_gen)
-
-    response_json = get_service_response('https://api.vk.com/method/groups.get', params)
     if response_json:
-        if 'error' in response_json:
-            print('Ошибка запроса к сервису ВК.')
-            print('Системная ошибка:', response_json['error']['error_msg'])
-        else:
-            groups_list = response_json['response']['items']
-            if not groups_list:
-                print('У выбранного пользователя нет групп ВК.')
+        groups_list = response_json['response']['items']
+        if not groups_list:
+            print('У выбранного пользователя нет групп ВК.')
 
     print(f'\nКоличество групп пользователя: {len(groups_list)}')
     return groups_list
@@ -92,23 +89,14 @@ def get_user_groups(user_id, progress_gen):
 def get_user_friends(user_id, progress_gen):
     '''Возвращает список друзей пользователя'''
     friends_list = None
-    params = {
-        'access_token': TOKEN,
-        'v': VERSION,
-        'user_id': user_id
-    }
+    spec_params = {'user_id': user_id}
+    params = {**base_params, **spec_params}
+    response_json = get_service_response(base_url + 'friends.get', params, progress_gen)
 
-    show_progress(progress_gen)
-
-    response_json = get_service_response('https://api.vk.com/method/friends.get', params)
     if response_json:
-        if 'error' in response_json:
-            print('Ошибка запроса к сервису ВК.')
-            print('Системная ошибка:', response_json['error']['error_msg'])
-        else:
-            friends_list = response_json['response']['items']
-            if not friends_list:
-                print('У выбранного пользователя нет друзей ВК.')
+        friends_list = response_json['response']['items']
+        if not friends_list:
+            print('У выбранного пользователя нет друзей ВК.')
 
     print(f'\nКоличество друзей пользователя: {len(friends_list)}')
     return friends_list
@@ -127,32 +115,15 @@ def is_there_any_friend(group_id, friends_list, progress_gen):
     for basket_number in range(baskets):
         user_list = [str(friends_list[counter]) for counter in range(basket_number, total_friends, baskets)]
         user_ids = ','.join(user_list)
-        params = {
-            'access_token': TOKEN,
-            'v': VERSION,
-            'group_id': group_id,
-            'user_ids': user_ids
-        }
-        for attempt_no in range(10):
-            show_progress(progress_gen)
-
-            response_json = get_service_response('https://api.vk.com/method/groups.isMember', params)
-            if response_json:
-                if 'error' in response_json:
-                    if response_json['error']['error_code'] == 6:
-                        time.sleep(0.5)
-                        continue
-                    else:
-                        print('Ошибка запроса к сервису ВК.')
-                        print('Системная ошибка:', response_json['error']['error_code'])
-                else:
-                    for item in response_json['response']:
-                        if item['member'] == 1:
-                            result = True
-                            break
-                    break
-            else:
-                break
+        spec_params = {'group_id': group_id, 'user_ids': user_ids}
+        params = {**base_params, **spec_params}
+        response_json = get_service_response(base_url + 'groups.isMember', params, progress_gen)
+        if response_json:
+            if not 'error' in response_json:
+                for item in response_json['response']:
+                    if item['member'] == 1:
+                        result = True
+                        break
         # Если нашли в группе хотя бы одного друга, дальше не ищем
         if result:
             break
